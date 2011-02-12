4d89a260270d001a5acd9d04e71c1ab571f1ea65
George Stephanos <gaf.stephanos@gmail.com>
2011-02-13 02:14:04 +0400

    Double-click plays a song. DLed/Total size of song is reported. Estimated bitrate. Speed calculated.

--- python/gui.py
+++ python/gui.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
+# -*- coding=utf-8 -*-
 import wx
 import wx.lib.newevent
 import groove
 import threading
 import urllib
 import os
+import sys
+import time
+import subprocess
 import ConfigParser
 from ObjectListView import ObjectListView, ColumnDefn
 
 def SetStatus(frame, event): frame.frame_statusbar.SetStatusText(event.attr1)
-def EnableFrame(frame, event): 
-  frame.txt_query.Enable(event.attr1)
-  frame.cb_type.Enable(event.attr1)
+def EnableFrame(frame, event):
+    frame.txt_query.Enable(event.attr1)
+    frame.cb_type.Enable(event.attr1)
+    frame.txt_query.SetFocus()
 def ClearResults(frame, event): frame.lst_results.DeleteAllItems() if frame.lst_results.GetItemCount() > 0 else None
 def ShowDownloader(frame, event):
     frame.lst_downloads.Show(event.attr1)
@@ -36,6 +41,9 @@ class MyFrame(wx.Frame):
         kwds["style"] = wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
         self.cb_type = wx.ComboBox(self, -1, choices=["Songs", "Artists", "Albums"], style=wx.CB_DROPDOWN|wx.CB_READONLY)
+        font = wx.Font(9, wx.FONTFAMILY_DEFAULT, style=wx.FONTSTYLE_NORMAL, weight=wx.FONTWEIGHT_NORMAL)
+        self.lbl_query = wx.StaticText(self, -1, "  Query:  ", style=wx.ALIGN_CENTRE)
+        self.lbl_query.SetFont(font)
         self.txt_query = wx.TextCtrl(self, 1, "", style=wx.TE_PROCESS_ENTER)
         self.lst_results = ObjectListView(self, -1, style=wx.LC_REPORT)
         self.lst_downloads = ObjectListView(self, -1, style=wx.LC_REPORT)
@@ -47,15 +55,17 @@ class MyFrame(wx.Frame):
         self.Bind(wx.EVT_TEXT_ENTER, self._TextEnter, self.txt_query)
         self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self._ResultsContext, self.lst_results)
         self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self._DoubleClick, self.lst_results)
+        self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self._DoubleClick, self.lst_downloads)
         self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self._DownloadsContext, self.lst_downloads)
 
         self.menu_results = {}
         self.menu_downloads = {}
         self.menu_results[ID_DOWNLOAD] = "Download"
         self.menu_downloads[ID_REMOVE] = "Remove"
-        
+
         icon = wx.Icon("groove.ico", wx.BITMAP_TYPE_ICO)
         self.SetIcon(icon)
+        del icon
 
     def __set_properties(self):
         self.SetTitle("JTR's Grooveshark Downloader")
@@ -76,7 +86,10 @@ class MyFrame(wx.Frame):
         self.lst_results._ResizeSpaceFillingColumns()
         columns = [
         ColumnDefn("Title", "left", 160, valueGetter = "filename", isSpaceFilling=True),
-        ColumnDefn("Progress", "center", 160, valueGetter = "progress")]
+        ColumnDefn("Estimated Bitrate", "center", 110, valueGetter = "bitrate"),
+        ColumnDefn("Speed", "center", 75, valueGetter = "speed"),
+        ColumnDefn("Done/Total", "center", 80, valueGetter = "size"),
+        ColumnDefn("Progress", "center", 80, valueGetter = "progress")]
         self.lst_downloads.SetColumns(columns)
         self.lst_downloads.SetObjects(self.downloads)
         self.lst_downloads.SetEmptyListMsg("N/A")
@@ -90,6 +103,7 @@ class MyFrame(wx.Frame):
         self.sizer_1 = wx.BoxSizer(wx.VERTICAL)
         self.sizer_2 = wx.BoxSizer(wx.HORIZONTAL)
         #self.sizer_2.Add(self.cb_type, 0, wx.EXPAND, 0)
+        self.sizer_2.Add(self.lbl_query, 0, wx.ALIGN_CENTER, 0)
         self.sizer_2.Add(self.txt_query, 2, 0, 0)
         self.sizer_1.Add(self.sizer_2, 0, wx.EXPAND, 0)
         self.sizer_1.Add(self.lst_results, 2, wx.EXPAND, 0)
@@ -118,13 +132,21 @@ class MyFrame(wx.Frame):
         self.PopupMenu(menu, event.GetPoint() + self.lst_downloads.GetPosition())
         menu.Destroy()
     def _DoubleClick(self, event):
-        self._ContextSelection(ID_DOWNLOAD)
+        if event.GetEventObject() == self.lst_results:
+            self._ContextSelection(ID_DOWNLOAD)
+        elif event.GetEventObject() == self.lst_downloads:
+            if sys.platform == 'win32':
+                path = dest + "\\" + self.lst_downloads.GetSelectedObjects()[0]["filename"]
+                os.startfile(path)
+            elif sys.platform == 'linux2':
+                path = dest + "/" + self.lst_downloads.GetSelectedObjects()[0]["filename"]
+                subprocess.Popen(['xdg-open', path])
 
     def _ContextSelection(self, event):
         if (event == ID_DOWNLOAD) or (event.GetId() == ID_DOWNLOAD):
             for song in self.lst_results.GetSelectedObjects():
                 filename = "%s - %s.mp3" % (strip(song["ArtistName"], "<>:\"/\|?*"), strip(song["SongName"], "<>:\"/\|?*"))
-                t = t_download(self, song["SongID"])
+                t = t_download(self, song)
                 t.download = {"progress":"Initializing", "thread":t, "filename":filename}
                 self.downloads.append(t.download)
                 self.lst_downloads.SetObjects(self.downloads)
@@ -136,16 +158,20 @@ class MyFrame(wx.Frame):
             self.lst_downloads.RemoveObjects(self.lst_downloads.GetSelectedObjects())
 
 class t_download(threading.Thread):
-    def __init__(self, frame, songid):
+    def __init__(self, frame, song):
         threading.Thread.__init__(self)
         self.frame = frame
-        self.songid = songid
+        self.songid = song["SongID"]
+        self.duration = int(song["EstimateDuration"])
         self.cancelled = False
     def run(self):
         key = groove.getStreamKeyFromSongIDEx(self.songid)
         try: os.makedirs(dest)
         except: pass
         try:
+            self.t = time.clock()
+            self.beg = self.t
+            self.lastCount = 0
             urllib.urlretrieve("http://" + key["result"]["ip"] + "/stream.php", dest + "/" + self.download["filename"], self.hook, "streamKey="+key["result"]["streamKey"])
         except Exception, ex:
             if ex.args[0] == "Cancelled":
@@ -156,6 +182,16 @@ class t_download(threading.Thread):
         if self.cancelled: raise Exception("Cancelled")
         progress = float(countBlocks*Block) / float(TotalSize) * 100
         self.download["progress"] = "%.0f%%" % progress if progress < 100 else "Completed"
+        self.download["bitrate"] = "%ukbps" % (TotalSize*8 / self.duration / 1000)
+        if time.clock() - self.t > 0.2:
+            self.download["size"] = "%.02f/%.02f MB" % (float(countBlocks*Block) / 1024**2, float(TotalSize) / 1024**2)
+            self.download["speed"] = "%.02f KB/s" % ((countBlocks - self.lastCount)*Block / (time.clock() - self.t) / 1024)
+            self.t = time.clock()
+            self.lastCount = countBlocks
+        if countBlocks*Block >= TotalSize:
+            self.download["size"] = "%.02f/%.02f MB" % (float(TotalSize) / 1024**2, float(TotalSize) / 1024**2)
+            self.download["speed"] = self.download["speed"] = "~%.02f KB/s" % (countBlocks*Block / (time.clock() - self.beg) / 1024)
+            
         wx.PostEvent(self.frame, evtExecFunc(func=UpdateItem, attr1=self.download))
 
 class t_search(threading.Thread):
