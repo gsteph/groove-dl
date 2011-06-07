02ea2a85bf6cd9f45e8d1abdc9a8dcb92d808d0f
George Stephanos <gaf.stephanos@gmail.com>
2011-06-07 14:09:30 +0200

    Download.svg, highslide

---
+++ highslide/highslide.config.js
@@ -0,0 +1,11 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'highslide/graphics/';
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.5;
+hs.dimmingDuration = 100;
+hs.easing = 'easeInBack';
+hs.easingClose = 'easeOutBack';
+hs.align = 'center';
+hs.captionEval = 'this.a.title';
